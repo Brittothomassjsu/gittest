11294b67fb7d192f8734e927b920d2e99c168d06
Brittothomassjsu <britto.thomas@sjsu.edu>
2017-09-09 16:07:01 -0700

    Update sampleccode.c

--- CMPE200/sampleccode.c
+++ CMPE200/sampleccode.c
@@ -3,7 +3,9 @@ int main()
 {
 	int num1 = 5;
 	int num2 = 7;
-	int sum = num1+num2;
-	printf("%d + %d = %d", num1, num2, sum);
+	int num3 = 9;
+	int sum = num1+num2+num3;
+	
+	printf("%d + %d + %d= %d", num1, num2, num4, sum);
 	return 0; 
 }
