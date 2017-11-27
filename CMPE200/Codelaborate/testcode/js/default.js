9a464b79de462908381df9be7300ffed4461cfe5
Britto Thomas <britto.thomas@sjsu.edu>
2017-11-26 21:31:54 -0800

    updating changes to include upload and download features

---
+++ CMPE200/Codelaborate/testcode/js/default.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	//code here...
+	var code = $(".codemirror-textarea")[0];
+	var editor = CodeMirror.fromTextArea(code, {
+		lineNumbers : true,
+		 tabMode: 'indent',
+    	lineNumbers: true,
+    	lineWrapping: true,
+   		autoCloseTags: true
+	});
+});
